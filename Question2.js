46c572f6ea4e390b4751f3f43103b8985650097b
A TO Z <sanad2000alawi@gmail.com>
2023-10-13 20:44:09 +0200

    Deploy

--- Question2.js
+++ Question2.js
@@ -1,12 +1,14 @@
 function sum(nums, target){
-    const map = {}
-
-    for(let i = 0; i < nums.length; i++){
-        let sub = target - nums[i]
-        if(map[sub]) return [map[sub], i]
-
-        map[nums[i]] = i;        
+    const map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]; 
+        
+        if (map.has(complement)) return [map.get(complement), i];
+        
+        
+        map.set(nums[i], i);
     }
-
-    return []
+    
+    return [];
 }
